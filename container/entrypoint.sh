ff68cdd3bcfa3f3e38c6e589e0ef5c759814d7fc
Robert Fenelon <robert.fenelon@globaleagle.com>
2019-10-01 19:54:46 +0000

    wget to keys to /tmp

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 OPTIONS=$@
-cp /etc/bind.keys /etc/bind/bind.keys
+cp /tmp/bind.keys /etc/bind/bind.keys
 chown -R root:named /etc/bind /var/run/named /var/log/named
 chown -R named:named /var/cache/bind
 chmod -R 770 /var/cache/bind /var/run/named /var/log/named
