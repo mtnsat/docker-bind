9b79f4a17c59e5d3fc79249235e2de0682c6671a
Robert Fenelon <robert.fenelon@globaleagle.com>
2019-10-01 19:44:38 +0000

    updates to permissions and key file download

--- container/entrypoint.sh
+++ container/entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 OPTIONS=$@
-chown -R root:named /etc/bind /var/run/named
+cp /etc/bind.keys /etc/bind/bind.keys
+chown -R root:named /etc/bind /var/run/named /var/log/named
 chown -R named:named /var/cache/bind
-chmod -R 770 /var/cache/bind /var/run/named
+chmod -R 770 /var/cache/bind /var/run/named /var/log/named
 chmod -R 750 /etc/bind
-
+rndc-confgen -a
 sync
 # Run in foreground and log to STDERR (console):
 /usr/sbin/named -c /etc/bind/named.conf -g -u named $OPTIONS
